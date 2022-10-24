6861ec5a711216df55f212e60f83c8a6cc7da510
crisandryj <crisandryj@gmail.com>
2022-10-24 12:11:19 -0400

    created airport model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# ActiveRecord::Base.transaction do
+#   Booking.destroy_all
+#   Passenger.destroy_all
+#   Flight.destroy_all
+#   Airport.destroy_all
