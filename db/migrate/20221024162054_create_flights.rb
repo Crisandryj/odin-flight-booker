1001950b53011c1777e0fa411163398fe0a48b3b
crisandryj <crisandryj@gmail.com>
2022-10-24 12:27:51 -0400

    added associations for flight and airport

--- db/migrate/20221024162054_create_flights.rb
+++ db/migrate/20221024162054_create_flights.rb
@@ -1,6 +1,11 @@
 class CreateFlights < ActiveRecord::Migration[7.0]
   def change
     create_table :flights do |t|
+      t.integer :departure_airport_id
+      t.integer :arrival_airport_id
+      t.datetime :start_date_time
+      t.integer :flight_duration
+
 
       t.timestamps
     end
