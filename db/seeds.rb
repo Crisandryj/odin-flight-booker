5e6e4413c59e00694b5dc62acc012054a71640e8
crisandryj <crisandryj@gmail.com>
2022-10-24 12:20:25 -0400

    created an array of airports for db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,16 @@
 #   Passenger.destroy_all
 #   Flight.destroy_all
 #   Airport.destroy_all
+
+
+airports = []
+airports[0] = Airport.create(code: 'PIT', name: 'Pittsburgh International Airport', city: 'Pittsburgh')
+airports[1] = Airport.create(code: 'SAN', name: 'San Diego International Airport', city: 'San Diego')
+airports[2] = Airport.create(code: 'DTW', name: 'Detroit Metropolitan Airport', city: 'Detroit')
+airports[3] = Airport.create(code: 'JFK', name: 'John F Kennedy International Airport', city: 'New York')
+airports[4] = Airport.create(code: 'SFO', name: 'San Francisco International Airport', city: 'San Francisco')
+airports[5] = Airport.create(code: 'ORD', name: 'O\'hare International Airport', city: 'Chicago')
+airports[6] = Airport.create(code: 'SLC', name: 'Salt Lake City International Airport', city: 'Salt Lake City')
+airports[7] = Airport.create(code: 'DFW', name: 'Dallas / Fort Worth International Airport', city: 'Dallas')
+airports[8] = Airport.create(code: 'SEA', name: 'Seattle-Tacoma International Airport', city: 'Seattle')
+airports[9] = Airport.create(code: 'BOS', name: 'Logan International Airport', city: 'Boston')
