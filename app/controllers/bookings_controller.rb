dcf1d629d26ce9566896b379bbc7d8c8baa96f0a
crisandryj <crisandryj@gmail.com>
2022-10-30 12:09:09 -0400

    added flight id to new booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,6 +2,7 @@ class BookingsController < ApplicationController
   def new
     @booking = Booking.new()
     @passengers = params[:passengers]
+    @flight_id = params[:flight_id][1]
   end
 
   def create
