28a4f1f58906d66e109a81d6b2d51cbc2227722c
crisandryj <crisandryj@gmail.com>
2022-10-31 08:06:50 -0400

    update

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,5 +3,5 @@ class Flight < ApplicationRecord
   belongs_to :arrival_airport, class_name: 'Airport'
   has_many :bookings
   has_many :passengers, through: :bookings
-  accepts_nested_attributes_for :passengers
+  
 end
