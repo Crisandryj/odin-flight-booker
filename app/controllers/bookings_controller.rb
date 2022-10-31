dd6ebc23fb9ec67c97695c44051ba64872523c1c
crisandryj <crisandryj@gmail.com>
2022-10-31 08:50:52 -0400

    added passenger build to booking controller and removed multiple passengers from booking view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,7 +4,7 @@ class BookingsController < ApplicationController
     @passengers = params[:passengers]
     @flight_id = params[:flight_id][1]
     @start_date_time = params[:start_date_time]
-
+    @passengers.to_i.times {@booking.passengers.build}
     # raise params.inspect
   end
 
