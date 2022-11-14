403de565349b94cf1ef604374167d04b9eb4b37c
crisandryj <crisandryj@gmail.com>
2022-11-14 10:39:03 -0500

    added welcome email action to mailer

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,9 @@
 class PassengerMailer < ApplicationMailer
+
+  def welcome_email
+    @passenger = params[:passenger]
+    @url = 'http://example.com/login'
+    mail(to: @passenger.email, subject: 'Flight has been booked')
+  end
+  
 end
