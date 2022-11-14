04582946a743d3eab0c0f4d007b00456bf774ca0
crisandryj <crisandryj@gmail.com>
2022-11-14 11:02:28 -0500

    added defualt to in Passenger mailer'

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -12,6 +12,20 @@ class BookingsController < ApplicationController
     # raise params.inspect
     @booking = Booking.new(booking_params)
     if @booking.save
+
+      respond_to do |format|
+      if @booking.save
+        # Tell the UserMailer to send a welcome email after save
+        UserMailer.with(user: @passenger).welcome_email.deliver_later
+
+        format.html { redirect_to(@passenger, notice: 'User was successfully created.') }
+        format.json { render json: @passenger, status: :created, location: @passenger }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @passenger.errors, status: :unprocessable_entity }
+      end
+    end
+
       redirect_to root_path, notice: "Congrats on your booking"
     else
       flash.now[:alert] = "Failed to book."
--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,9 +1,11 @@
 class PassengerMailer < ApplicationMailer
+  default to: -> { Passenger.pluck(:email) },
+        from: 'notifications@example.com'
 
   def welcome_email
     @passenger = params[:passenger]
     @url = 'http://example.com/login'
     mail(to: @passenger.email, subject: 'Flight has been booked')
   end
-  
+
 end
